71f7d98d5ea9f81f91d82cac39ed6f8082f05efc
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-03-31 21:08:04 -0300

    Feat: deserialize function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,11 +10,46 @@ let task1 = new Task('Task de teste', '2005-25-06', 0)
 
 let step1 = new Step('Step de teste')
 
-step1.changeStatus();
+project1.addTask(task1);
 task1.addStep(step1);
 
-project1.addTask(task1)
+let projects = [];
 
-console.log(project1);
+projects.push(project1);
 
-console.log(project1.logProject());
+let project2 = project1;
+projects.push(project2);
+
+let stringifiedProjects = JSON.stringify(projects);
+
+
+function deserializeProjects(jsonString) {
+  let jsonData = JSON.parse(jsonString);
+
+  return jsonData.map(projectData => {
+    const project = new Project(projectData._title, projectData._description, projectData._dueDate, projectData._priority);
+
+    project.projectTasks = projectData._projectTasks.map(taskData => {
+      const task = new Task(taskData._title, taskData._dueDate, taskData._progress);
+
+      task.taskSteps = taskData._taskSteps.map(stepData => new Step(stepData._title, stepData._status))
+
+      return task;
+    })
+
+    return project;
+  })
+}
+
+console.log(stringifiedProjects);
+// console.log(reparsedProjects);
+
+projects = deserializeProjects(stringifiedProjects);
+
+console.log(projects)
+
+projects[0].title = 'Titulo mudado via set';
+
+projects[0].projectTasks[0]._progress = 69;
+
+console.log(projects)
